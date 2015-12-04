f407eaa39dcf897cfe741b8c09284128c1985dbf
Wayne Maurer <wayne_maurer@yahoo.com.au>
2015-12-04 22:34:45 +0100

    tried to implement indenting after converting jade to html, didn't work so well ...

--- extension.ts
+++ extension.ts
@@ -18,10 +18,13 @@ export function activate(context: vscode.ExtensionContext) {
 		const promises = textEditor.selections.map(selection => {
 			const text = textEditor.document.getText(selection);
 			return convertHtml(text, convertHtmlOptions)
-				.then(replacementText => {
+				.then(jadeText => {
+					const replacementText = jadeText.substr(0, jadeText.length - 1).split('\n')
+						.filter(line => !line.match(/\s*\|\s*/))
+						.reduce((p, c) => p + '\n' + c, '');
 					return {
 						selection,
-						replacementText: replacementText.substr(0, replacementText.length - 1)
+						replacementText
 					};
 				});
 		});
@@ -36,10 +39,25 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(html2JadeDisposable);
 
 	const jade2HtmlDisposable = vscode.commands.registerTextEditorCommand('extension.jade2html', (textEditor, edit) => {
+		const replaceFn = textEditor.options.insertSpaces
+			? indents => Array(indents * textEditor.options.tabSize + 1).join(' ')
+			: indents => Array(indents + 1).join('\t');
 
+		const regex = /^(  )+/;
 		const selectionsTransformed = textEditor.selections.map(selection => {
 			const text = textEditor.document.getText(selection);
-			const replacementText = jade.compile(text, { pretty: true })();
+			const htmlText = jade.compile(text, { pretty: true })() as string;
+			const replacementText = htmlText.split('\n')
+				.map(line => {
+					const matches = line.match(regex);
+					return {
+						line,
+						indents: !matches ? 0 : matches[0].length / 2
+					};
+				})
+				.map(x => x.line.replace(regex, replaceFn(x.indents)))
+				// .map(x => x.line)
+				.reduce((p, c) => p + '\n' + c, '');
 			return {
 				selection,
 				replacementText
@@ -51,7 +69,9 @@ export function activate(context: vscode.ExtensionContext) {
 				editBuilder.replace(x.selection, x.replacementText);
 			});
 		});
+
+		// vscode.commands.executeCommand('editor.action.format');
 	});
-	context.subscriptions.push(html2JadeDisposable);
+	context.subscriptions.push(jade2HtmlDisposable);
 
 }
